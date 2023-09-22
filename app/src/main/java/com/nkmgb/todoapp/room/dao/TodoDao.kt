2a6de5cd360c52be644c9d1672f3c378294e02b3
nithinkumarm <nithin_kumarm@discovery.com>
2023-09-22 10:43:27 +0530

    Room

---
+++ app/src/main/java/com/nkmgb/todoapp/room/dao/TodoDao.kt
@@ -0,0 +1,28 @@
+package com.nkmgb.todoapp.room.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.nkmgb.todoapp.room.database.TodoItem
+
+@Dao
+interface TodoDao {
+    @Query("SELECT * FROM todolist")
+    fun getTodoList(): List<TodoItem>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addTodo(todoItem: TodoItem)
+
+    @Query("SELECT * FROM todolist WHERE id = :id")
+    fun findEmployeeById(id: Int): TodoItem
+
+    @Update
+    suspend fun updateTodo(todoItem: TodoItem)
+
+    @Delete
+    suspend fun deleteTodoItem(todoItem: TodoItem)
+
+}
